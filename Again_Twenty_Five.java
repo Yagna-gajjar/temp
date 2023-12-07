b6493b85179323a609332bfb1d99f3d89222e050
Yagna-gajjar <yagnagangajaliya@gmail.com>
2023-12-07 09:25:54 +0530

    hii

---
+++ Again_Twenty_Five.java
@@ -0,0 +1,13 @@
+import java.util.*;
+/**
+ * Again_Twenty_Five
+ */
+public class Again_Twenty_Five {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println("25");
+        
+    }
+}
